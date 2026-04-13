5eb9c0cacc11975251f88e3d45dfb5f5134b122f
je26011997bee27-mepco <je26011997_bee27@mepcoeng.ac.in>
2026-04-13 11:50:45 +0530

    Implement maxDepth function for binary tree

---
+++ 5.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+        return left + 1 if left > right else right + 1
